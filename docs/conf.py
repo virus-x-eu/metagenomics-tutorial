1134f9e77c0dfea568401d4b37e690d7b22cc7eb
Christian Henke <christian@cebitec.uni-bielefeld.de>
2018-08-31 16:05:26 +0200

    fix

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Metagenomics Workshop documentation build configuration file, created by
+# Metagenomics Tutorial documentation build configuration file, created by
 # sphinx-quickstart on Fri Sep 11 18:48:58 2015.
 #
 # This file is execfile()d with the current directory set to its
@@ -44,7 +44,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'Metagenomics Workshop'
+project = 'Metagenomics Tutorial'
 copyright = '2015, Alex Sczyrba'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -177,7 +177,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'MetagenomicsWorkshopdoc'
+htmlhelp_basename = 'MetagenomicsTutorialdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -197,7 +197,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'MetagenomicsWorkshop.tex', 'Metagenomics Workshop Documentation',
+  ('index', 'MetagenomicsTutorial.tex', 'Metagenomics Tutorial Documentation',
    'Alex Sczyrba', 'manual'),
 ]
 
@@ -227,7 +227,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'metagenomicsworkshop', 'Metagenomics Workshop Documentation',
+    ('index', 'metagenomicstutorial', 'Metagenomics Tutorial Documentation',
      ['Alex Sczyrba'], 1)
 ]
 
@@ -241,8 +241,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'MetagenomicsWorkshop', 'Metagenomics Workshop Documentation',
-   'Alex Sczyrba', 'MetagenomicsWorkshop', 'One line description of project.',
+  ('index', 'MetagenomicsTutorial', 'Metagenomics Tutorial Documentation',
+   'Alex Sczyrba', 'MetagenomicsTutorial', 'One line description of project.',
    'Miscellaneous'),
 ]
 
@@ -262,13 +262,13 @@ texinfo_documents = [
 # -- Options for Epub output ----------------------------------------------
 
 # Bibliographic Dublin Core info.
-epub_title = 'Metagenomics Workshop'
+epub_title = 'Metagenomics Tutorial'
 epub_author = 'Alex Sczyrba'
 epub_publisher = 'Alex Sczyrba'
 epub_copyright = '2015, Alex Sczyrba'
 
 # The basename for the epub file. It defaults to the project name.
-#epub_basename = 'Metagenomics Workshop'
+#epub_basename = 'Metagenomics Tutorial'
 
 # The HTML theme for the epub output. Since the default themes are not optimized
 # for small screen space, using the same theme for HTML and epub output is
